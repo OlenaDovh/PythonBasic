a28c7fa8b1149f21c2b94f339dd192c200e3f22c
Olenadovh <lothenshow@gmail.com>
2025-10-27 09:58:15 +0200

    task 2.1 and 2.2 are edited

--- task2.1.py
+++ task2.1.py
@@ -1,7 +1,7 @@
 initial_number = int(input("Enter 4-digit number "))
 
-first_num, fourth_num = divmod(initial_number, 10)
-first_num, third_num = divmod(first_num, 10)
-first_num, second_num = divmod(first_num, 10)
+first_digit, fourth_digit = divmod(initial_number, 10)
+first_digit, third_digit = divmod(first_digit, 10)
+first_digit, second_digit = divmod(first_digit, 10)
 
-print(first_num, second_num, third_num, fourth_num, sep="\n")
+print("Your number in a column:", first_digit, second_digit, third_digit, fourth_digit, sep="\n")
--- task2.2.py
+++ task2.2.py
@@ -1,8 +1,15 @@
 initial_number = int(input("Enter 5-digit number "))
 
-first_num, fifth_num = divmod(initial_number, 10)
-first_num, fourth_num = divmod(first_num, 10)
-first_num, third_num = divmod(first_num, 10)
-first_num, second_num = divmod(first_num, 10)
+initial_number, fifth_num = divmod(initial_number, 10)
+fifth_num *= 10000
+initial_number, fourth_num = divmod(initial_number, 10)
+fourth_num *= 1000
+initial_number, third_num = divmod(initial_number, 10)
+third_num *= 100
+initial_number, second_num = divmod(initial_number, 10)
+second_num *= 10
+initial_number, first_num = divmod(initial_number, 10)
 
-print(fifth_num, fourth_num, third_num, second_num, first_num, sep="")
+reversed_num = first_num + second_num + third_num + fourth_num + fifth_num
+
+print("Reversed number is", reversed_num)
